5a7882f514c8a411dd6019e6e442b18ab50d062f
juandavidac <jd.ac@outlook.com>
2016-02-02 09:10:11 -0500

    v9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def authenticate_user_from_token!
     user_email=params[:user_email].presence
     user = user_email && User.find_by_email(user_email)
-    if user && Devise.secure_compare(user.authentication_token, params[:user_token])
+    if user && Devise.secure_compare(user.api_token, params[:user_token])
       sign_in user, store:false
     end
   end
