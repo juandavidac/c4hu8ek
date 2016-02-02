6b67dd940452a9d1c0b303b2a3aa4272f4348a9a
juandavidac <jd.ac@outlook.com>
2016-02-02 09:18:49 -0500

    v11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :athenticate_user_from_token!
+  before_filter :authenticate_user_from_token!
   before_filter :authenticate_user!
 
   private
